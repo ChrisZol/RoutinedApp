d788ae4a1f70cadbfb0de98e85ab5148075dd278
ChrisZolk <christiane.zolkin@fh-bielefeld.de>
2022-05-20 20:27:46 +0200

    Added table relationships in init.sql

--- routined_app/server/database/init.sql
+++ routined_app/server/database/init.sql
@@ -1,20 +1,24 @@
 CREATE TABLE IF NOT EXISTS user_accounts (
-    id INTEGER PRIMARY KEY,
+    user_id INTEGER PRIMARY KEY,
     username TEXT NOT NULL,
     email TEXT NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS routines (
-    id INTEGER PRIMARY KEY,
+    routine_id INTEGER PRIMARY KEY,
     name TEXT NOT NULL,
     occurence TEXT CHECK(occurence IN('Daily', 'Weekly', 'Monthly', 'Yearly')) NOT NULL,
     start_time NOT NULL,
-    end_time NOT NULL
+    end_time NOT NULL,
+    user_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user_accounts (user_id)
 );
 
 CREATE TABLE IF NOT EXISTS tasks (
-    id INTEGER PRIMARY KEY,
+    task_id INTEGER PRIMARY KEY,
     name TEXT NOT NULL,
     duration INTEGER NOT NULL,
-    description TEXT NOT NULL
+    description TEXT NOT NULL,
+    routine_id INTEGER NOT NULL,
+    FOREIGN KEY (routine_id) REFERENCES routines (routine_id)
 );
